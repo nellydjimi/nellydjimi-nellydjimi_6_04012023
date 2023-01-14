c685ee59510bfd382f1cf4c5ab67bb7308690354
nellydjimi <djiminelly@gmail.com>
2023-01-14 04:19:23 +0100

    init

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,18 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res) => {
-   res.json({ message: 'Votre requête a bien été reçue !' }); 
-});
+app.use(express.json()); //intersepte tout les objet json
 
-module.exports = app; 
+//CORS middleware 
+app.use((req, res, next) => {
+   res.setHeader('Access-Control-Allow-Origin', '*'); //d'accéder à notre API depuis n'importe quelle origine
+   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); //d'ajouter les headers mentionnés aux requêtes envoyées vers notre API
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); //d'envoyer des requêtes avec les méthodes mentionnées 
+   next();
+ });
+
+app.post("/api/auth/signup");
+app.post("/api/auth/login");
+
+
+module.exports = app;
