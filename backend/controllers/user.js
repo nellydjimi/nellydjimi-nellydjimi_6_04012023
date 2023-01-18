1807af3bc725f660c16d4a6c52764ba68ea0282e
nellydjimi <djiminelly@gmail.com>
2023-01-18 16:42:27 +0100

    maj

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,14 +1,42 @@
 const user = require('../models/users')
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 exports.signup = (req, res, next) => {
-bcrypt.hash(req.body.password, 10)
-.then(hash =>{
-
-    })
-.catch
+    bcrypt.hash(req.body.password, 10)
+      .then(hash => {
+        const user = new User({
+          email: req.body.email,
+          password: hash
+        });
+        user.save()
+          .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+          .catch(error => res.status(400).json({ error }));
+      })
+      .catch(error => res.status(500).json({ error }));
 };
 
 exports.login = (req, res, next) => {
-
+    User.findOne({ email: req.body.email })
+    .then(user => {
+        if (!user) {
+            return res.status(401).json({ message: 'Utilisateur introuvable'});
+        }
+        bcrypt.compare(req.body.password, user.password)
+            .then(valid => {
+                if (!valid) {
+                    return res.status(401).json({ message: 'Mot de passe incorrecte' });
+                }
+                res.status(200).json({
+                    userId: user._id,
+                    token: jwt.sign(
+                        {userId: user._id},
+                        process.env.MY_Token,
+                        {expiresIn: '24h'}
+                    )
+                });
+            })
+            .catch(error => res.status(500).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
 };
