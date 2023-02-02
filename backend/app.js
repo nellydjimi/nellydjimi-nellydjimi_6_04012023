08bab35ce1bc447991ffe58ca091e8c4456d9de1
nellydjimi <djiminelly@gmail.com>
2023-02-02 17:45:01 +0100

    maj

--- backend/app.js
+++ backend/app.js
@@ -4,19 +4,24 @@ const app = express();
 const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 const path = require('path');
+const helmet = require('helmet');
+require('dotenv').config();
+
 
 //connexion MongoDB
-mongoose.connect('mongodb+srv://nellydjimi:JrcWxHI3pK6Qe7fF@cluster0.z3ongsa.mongodb.net/?retryWrites=true&w=majority',
+mongoose.connect(process.env.DB_CONNECT,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch((error) => console.log(error));
 //JrcWxHI3pK6Qe7fF
 
+app.use(helmet());
 //CORS middleware 
 app.use((req, res, next) => {
    res.setHeader('Access-Control-Allow-Origin', '*'); //d'accéder à notre API depuis n'importe quelle origine
    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); //d'ajouter les headers mentionnés aux requêtes envoyées vers notre API
+   res.setHeader('Cross-Origin-Resource-Policy', 'same-site');
    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); //d'envoyer des requêtes avec les méthodes mentionnées 
    next();
  });
--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -76,11 +76,80 @@ exports.getSauces = ( (req, res, next) => {
 
 //defini le statut like avec l'userId
   exports.likeSauces = ( (req, res, next) => {
-    Sauces.updateOne(
-        {_id: req.params.id}, 
-        {$push: {usersLiked: req.body.userId},
-        $inc: {likes: +1}}
-    )
-    .then(() => { res.status(200).json({message: 'Votre sauce a été supprimé !'})})
-    .catch(error => res.status(400).json({ error }));
+    let like = req.body.like
+    // On prend le userID
+    let userId = req.body.userId
+    // On prend l'id de la sauce
+    let sauceId = req.params.id
+  
+    if (like === 1) { 
+      Sauces.updateOne({
+          _id: sauceId
+        }, {
+          $push: { usersLiked: userId},
+          $inc: {likes: +1 }, 
+        })
+        .then(() => res.status(200).json({
+          message: 'Vous aimez cette sauce'
+        }))
+        .catch((error) => res.status(400).json({
+          error
+        }))
+    }
+    if (like === -1) {
+      Sauces.updateOne( 
+          { _id: sauceId}, 
+          {
+            $push: {usersDisliked: userId },
+            $inc: { dislikes: +1}, 
+          }
+        )
+        .then(() => {
+          res.status(200).json({
+            message: "Vous n'aimez pas cette sauce"
+          })
+        })
+        .catch((error) => res.status(400).json({
+          error
+        }))
+    }
+    if (like === 0) { 
+      Sauces.findOne({
+          _id: sauceId
+        })
+        .then((sauce) => {
+          if (sauce.usersLiked.includes(userId)) { 
+            Sauces.updateOne({
+                _id: sauceId
+              }, { $pull: {
+                  usersLiked: userId},
+                $inc: { likes: -1 }, 
+              })
+              .then(() => res.status(200).json({
+                message: 'Like retiré !'
+              }))
+              .catch((error) => res.status(400).json({
+                error
+              }))
+          }
+          //retirer dislike
+          if (sauce.usersDisliked.includes(userId)) { 
+            Sauces.updateOne({
+                _id: sauceId
+              }, {
+                $pull: { usersDisliked: userId },
+                $inc: { dislikes: -1 }, 
+              })
+              .then(() => res.status(200).json({
+                message: 'Dislike retiré !'
+              }))
+              .catch((error) => res.status(400).json({
+                error
+              }))
+          }
+        })
+        .catch((error) => res.status(404).json({
+          error
+        }))
+    }
   });
